72a81f7a58feb1081b2164c3600a56f5d4c02b1b
hexlive <30753691+hexlive@users.noreply.github.com>
2017-08-07 01:45:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.get('/', function (req, res) {
 app.get('/article one',function(req, res){
    res.send('Article one will be serve here');
 });
+app.get('/article two',function(req, res){
+   res.send('Article two will be serve here');
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
